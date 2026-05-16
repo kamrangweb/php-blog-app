8f4e034286e8ff23e93d021a73c38177310da258
Kamran Gasimov <92179625+kamrangweb@users.noreply.github.com>
2026-05-16 21:28:10 +0400

    Enhance NotFoundException with default message and code

--- app/Exceptions/NotFoundException.php
+++ app/Exceptions/NotFoundException.php
@@ -5,20 +5,25 @@ namespace App\Exceptions;
 use Exception;
 use Throwable;
 
-
 class NotFoundException extends Exception
 {
-
-    public function __construct($message = "", $code = 0, ?Throwable $previous = null)
-    {
+    public function __construct(
+        string $message = "Page not found",
+        int $code = 404,
+        ?Throwable $previous = null
+    ) {
         parent::__construct($message, $code, $previous);
     }
 
-
-    public function error404()
+    /**
+     * Render 404 page
+     */
+    public function render(): void
     {
         http_response_code(404);
 
-        return require VIEWS_FOLDER . 'errors/404.view.php';
+        require VIEWS_FOLDER . 'errors/404.view.php';
+
+        exit;
     }
 }
